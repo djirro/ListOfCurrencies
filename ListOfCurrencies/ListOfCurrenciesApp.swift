d2aa7918484e39b50c3101e4d92219c5606a6e32
Egor Danshov <djirrochannel@gmail.com>
2021-07-20 00:29:56 -0700

    Initial Commit

---
+++ ListOfCurrencies/ListOfCurrenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfCurrenciesApp.swift
+//  ListOfCurrencies
+//
+//  Created by Егор Даншов on 20/7/21.
+//
+
+import SwiftUI
+
+@main
+struct ListOfCurrenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
