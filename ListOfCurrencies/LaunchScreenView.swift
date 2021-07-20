8fbd30f69b45570a4d577490bada16253caf8747
Egor Danshov <djirrochannel@gmail.com>
2021-07-20 06:35:21 -0700

    Добавление иконок и LaunchScreen

---
+++ ListOfCurrencies/LaunchScreenView.swift
@@ -0,0 +1,22 @@
+//
+//  LaunchScreenView.swift
+//  ListOfCurrencies
+//
+//  Created by Егор Даншов on 20/7/21.
+//
+
+import SwiftUI
+import Foundation
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct LaunchScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreenView()
+            .environment(\.sizeCategory, .medium)
+    }
+}
